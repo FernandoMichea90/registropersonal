b9ffdba263e2ceee068b51c6a2ce8cbbb3466551
FernandoMichea90 <fmicheam@gmail.com>
2022-06-19 05:37:57 -0400

    dashboard y login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './Pages/Login/Login';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        esta es una prueba hola mundo  fernando michea  
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fernando Javier Michea Marquez 
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/" element={<Login />}></Route>
+    </Routes>
+  </BrowserRouter>
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //     esta es una prueba hola mundo  fernando michea  
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Fernando Javier Michea Marquez 
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
